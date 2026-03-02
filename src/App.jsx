caf1b1d4a48ee4e03f5eeb391a348e8e005d6c80
Op0778 <prakashlkom5@gmail.com>
2026-03-02 11:16:58 +0530

    Login and Register solved

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,39 @@
 import { useState } from "react";
 import Login from "./frontend/auth/Login";
 import Register from "./frontend/auth/Register";
+import { Routes, Route } from "react-router-dom";
+import Home from "./frontend/pages/Home";
+import { Navigate } from "react-router-dom";
 
 function App() {
-  const [isLogin, setIsLogin] = useState(true);
+  const [token, setToken] = useState(localStorage.getItem("token"));
+
+  const handleLogin = (t, id, role) => {
+    setToken(t);
+    localStorage.setItem("token", t);
+    localStorage.setItem("userId", id);
+    localStorage.setItem("role", role);
+  };
+
+  const handleLogout = () => {
+    setToken(null);
+    localStorage.removeItem("token");
+    localStorage.removeItem("userId");
+    localStorage.removeItem("role");
+  };
 
   return (
-    <div className="container">
-      {isLogin ? (
-        <Login switchToRegister={() => setIsLogin(false)} />
-      ) : (
-        <Register switchToLogin={() => setIsLogin(true)} />
-      )}
+    <div className="">
+      <Routes>
+        <Route path="/" element={<Login onLogin={handleLogin} />} />
+        <Route path="/register" element={<Register />} />
+        <Route
+          path="/home"
+          element={
+            token ? <Home onLogout={handleLogout} /> : <Navigate to="/" />
+          }
+        />
+      </Routes>
     </div>
   );
 }
---
+++ src/frontend/pages/Home.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Home = () => {
+  return <div>welcom to my ai</div>;
+};
+
+export default Home;
